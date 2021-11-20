a8fe8160b1184d94f090ec6f9fda6676576a62e3
Maarten Jan van der Meulen <mjvandermeulen@gmail.com>
2021-11-19 23:25:08 -0500

    added css hardcoded html grid. show answers transition working.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
 let syntaxLitHigh = false;
 let exampleCode = "";
 
+// NEW GRID CODE
+
+function answer(method) {
+  document
+    .querySelectorAll(`#challenge .method-${method}.ans-wrapper`)
+    .forEach((el) => {
+      el.classList.toggle("ans-collapsed");
+    });
+}
+
+// END NEW GRID CODE
+
 function showResult(key) {
   s = document.getElementById(key);
   s.innerHTML = results[key].list;
 }
 
+// ******* cleanup
+
+let showRes = false;
+
+function show(result) {
+  console.log("show called");
+  showRes = !showRes;
+  if (result == "res1") {
+    resDivs = document.querySelectorAll("div#test");
+    resDivs.forEach((element) => {
+      element.style.maxHeight = showRes ? "100px" : "0";
+    });
+  }
+}
+
 function createResultsButtons() {
   for (const [key, method] of Object.entries(results)) {
-    rl = document.getElementById("results");
+    rl = document.getElementById("OLDRESULTS");
 
     li = document.createElement("li");
     li.innerHTML = `
